12022492540d2e228046d2291932c876e62896d3
Dougie1993 <dkadzutu@gmail.com>
2021-06-10 08:08:56 +0200

    cors enabled

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ const List = require('./db/models/list.model')
 /* Load Middleware */
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+/*Enable Cors */
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 /* Routes */
 app.get('/', (req, res) => {
     res.send('Hello World')
