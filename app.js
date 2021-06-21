9c803cfbb99754aa3f021560b3ebf0e183627345
Dougie1993 <dkadzutu@gmail.com>
2021-06-21 23:21:41 +0200

    expose x-acces-token in headers

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
     res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "x-access-token, x-refresh-token");
     // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PATCH, PUT, GET,POST, DELETE');
